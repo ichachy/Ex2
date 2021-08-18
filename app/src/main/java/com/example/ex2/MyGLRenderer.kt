3bd37ff55a24529643d2841e8376fec74a369a68
ichachy <ichachy.lightricks.com>
2021-08-18 11:25:17 +0300

    Add companion object
    Companion object was added as load shader to Renderer class

--- app/src/main/java/com/example/ex2/MyGLRenderer.kt
+++ app/src/main/java/com/example/ex2/MyGLRenderer.kt
@@ -61,4 +61,23 @@ class MyGLRenderer: GLSurfaceView.Renderer {
         triangle.draw(scratch)
     }
 
+    companion object {
+        fun loadShader(type: Int, shaderCode: String): Int {
+
+            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+            return GLES20.glCreateShader(type).also { shader ->
+
+                // add the source code to the shader and compile it
+                GLES20.glShaderSource(shader, shaderCode)
+                GLES20.glCompileShader(shader)
+
+                val compiled = IntArray(1)
+                GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0)
+                if (compiled[0] == 0) {
+                    println("Could not compile shader $type: " + GLES20.glGetShaderInfoLog(shader))
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/ex2/Triangle.kt
+++ app/src/main/java/com/example/ex2/Triangle.kt
@@ -46,7 +46,7 @@ class Triangle {
                 "}"
 
 
-    private var verticesPositionsBuffer: FloatBuffer =
+    private val verticesPositionsBuffer: FloatBuffer =
         // (number of coordinate values * 4 bytes per float)
         ByteBuffer.allocateDirect(verticesPositions.size * Float.SIZE_BYTES).run {
 
@@ -62,7 +62,7 @@ class Triangle {
             }
         }
 
-    private var verticesColorsBuffer: FloatBuffer =
+    private val verticesColorsBuffer: FloatBuffer =
         // (number of coordinate values * 4 bytes per float)
         ByteBuffer.allocateDirect(verticesColors.size * Float.SIZE_BYTES).run {
 
@@ -82,8 +82,8 @@ class Triangle {
     private var mProgram: Int
 
     init {
-        val vertexShader: Int = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
-        val fragmentShader: Int = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
+        val vertexShader: Int = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode)
+        val fragmentShader: Int = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode)
 
         // create empty OpenGL ES Program
         mProgram = GLES20.glCreateProgram().also {
@@ -96,24 +96,6 @@ class Triangle {
 
             // creates OpenGL ES program executables
             GLES20.glLinkProgram(it)
-        }
-    }
-
-    private fun loadShader(type: Int, shaderCode: String): Int {
-
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-        return GLES20.glCreateShader(type).also { shader ->
-
-            // add the source code to the shader and compile it
-            GLES20.glShaderSource(shader, shaderCode)
-            GLES20.glCompileShader(shader)
-
-            val compiled = IntArray(1)
-            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0)
-            if (compiled[0] == 0) {
-                println("Could not compile shader $type: " + GLES20.glGetShaderInfoLog(shader))
-            }
         }
     }
 
